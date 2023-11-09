be898aa2bbed74c1b4d7b721157683977c018790
RODHOLFO_EBAC <ro.garo87@gmail.com>
2023-11-09 11:41:34 -0300

    Duvida sobre Modularização JS

---
+++ gulpfile.js
@@ -0,0 +1,14 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+
+function compilaSass(){
+    return gulp.src('./src/styles/*.scss')
+
+    .pipe(sass({
+        outputStyle:'compressed'
+    }))
+    .pipe(gulp.dest('./build/styles'));
+}
+
+exports.default = compilaSass;
+
